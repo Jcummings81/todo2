70a4bf44387f6059ef3c72774e0b0a3885f0b3b9
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-29 17:10:53 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { 
+      todos: [
+        { id: 1, name: 'Learn Ninja Stuff', complete: true },
+        { id: 2, name: 'Get Ninja gear', complete: true },
+        { id: 3, name: 'Master Wu Tang', complete: true },
+        { id: 4, name: 'Kill the Emperor', complete: true },
+      ]
+   }
+
+  render() {
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
